ef56dcce3d4cba08f7f4d1a09a000567adf5e4d0
Dehzxg <andre.santos610@outlook.com>
2021-11-29 14:34:06 -0300

    Commit Inicial

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+            <h1 className="Titulo">Home</h1>
+            <img src="https://imgur.com/gallery/U2Z32fi" alt="teste de imagem" className="img"/>
+        </>
+    );
+}
+
+export default Home;
